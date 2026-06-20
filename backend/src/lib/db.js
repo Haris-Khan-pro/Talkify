b7cf2b32db006e6b48ac9b818532d1e35fa29a48
Haris Khan <hariskhan01.gsm@gmail.com>
2026-06-20 22:34:31 +0500

    Add user and message models, database connection, and integrate Clerk middleware

---
+++ backend/src/lib/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+export async function connectDB() {
+  try {
+    const mongoUri = process.env.MONGO_URI;
+
+    if (!mongoUri) {
+      throw new Error("👎🏻 Hmm MONGO_URI is required");
+    }
+
+    const conn = await mongoose.connect(mongoUri);
+    console.log("👍 MongoDB connected successfully", conn.connection.host);
+  } catch (error) {
+    console.error("💥 MongoDB connection error", error.message);
+    process.exit(1); // 1 means failure and 0 means successful
+  }
+}
